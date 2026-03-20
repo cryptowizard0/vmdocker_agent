5130cab307dca7cbbb2dc059ade6ac0eb404f659
Potter <45678072+cryptowizard0@users.noreply.github.com>
2026-03-20 08:59:35 +0800

    Consolidate agent image build flow

--- docker_run.sh
+++
@@ -1,8 +0,0 @@
-# openclaw runtime
-docker run --name hymatrix-openclaw \
-    -d \
-    -p 8080:8080 \
-    -p 18789:18789 \
-    -e RUNTIME_TYPE=openclaw \
-    -e OPENCLAW_GATEWAY_URL=http://127.0.0.1:18789 \
-    chriswebber/docker-openclaw:latest
